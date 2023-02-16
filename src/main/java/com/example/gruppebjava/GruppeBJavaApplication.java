6a623df47cf0af13400d75e4b4f5dc27ae572a22
th62 <haberland@webtaktik.de>
2023-02-16 12:46:53 +0100

    Initial commit

---
+++ src/main/java/com/example/gruppebjava/GruppeBJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.gruppebjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GruppeBJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GruppeBJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gruppebjava/GruppeBJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gruppebjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GruppeBJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
