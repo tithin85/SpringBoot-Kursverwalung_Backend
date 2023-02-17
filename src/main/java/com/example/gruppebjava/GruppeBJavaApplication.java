16682bcab8a81aa73c40e4d124e94831bb53479d
Tithin <ttomy@uni-bremen.de>
2023-02-17 10:02:10 +0100

    für renate

--- src/main/java/com/example/gruppebjava/GruppeBJavaApplication.java
+++ src/main/java/com/example/gruppebjava/GruppeBJavaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GruppeBJavaApplication {
+    //GruppeBJavaApplication
 
     public static void main(String[] args) {
         SpringApplication.run(GruppeBJavaApplication.class, args);
