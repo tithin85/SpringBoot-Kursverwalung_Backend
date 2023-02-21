afc2e1f05944167a0bb58b6e751649849aafc0fc
AliK <karakose_ali@hotmail.com>
2023-02-21 10:34:58 +0100

    get all und findbyid sind fertig

--- src/main/java/com/example/gruppebjava/core/repo/KursRepo.java
+++ src/main/java/com/example/gruppebjava/core/repo/KursRepo.java
@@ -3,5 +3,14 @@ package com.example.gruppebjava.core.repo;
 import com.example.gruppebjava.core.domain.KursEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface KursRepo extends JpaRepository<KursEntity,Long> {
+
+    KursEntity findKursEntityById(Long id);
+
+
+    void deleteKursEntityById(Long id);
+
+
 }
--- src/main/java/com/example/gruppebjava/core/service/KursService.java
+++ src/main/java/com/example/gruppebjava/core/service/KursService.java
@@ -5,19 +5,40 @@ import com.example.gruppebjava.core.repo.KursRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
 public class KursService {
     private final KursRepo kursRepo;
 
+
     @Autowired
     public KursService(KursRepo kursRepo) {
         this.kursRepo = kursRepo;
+
     }
 
     public KursEntity addKurs(KursEntity kurs){
         kurs.setKursCode(UUID.randomUUID().toString());
         return kursRepo.save(kurs);
     }
+
+    public List<KursEntity> findAllKurse(){
+        return kursRepo.findAll();
+    }
+
+    public KursEntity findKursById(Long id){
+        return kursRepo.findKursEntityById(id);
+    }
+
+    public KursEntity updateKurs(KursEntity kurs){
+        return kursRepo.save(kurs);
+    }
+
+    public void deleteKurs(Long id){
+        kursRepo.deleteKursEntityById(id);
+    }
+
+
 }
