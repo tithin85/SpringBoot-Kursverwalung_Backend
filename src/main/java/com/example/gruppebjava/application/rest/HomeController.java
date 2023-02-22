a48f22d949b1bf1a5b17cf4b97c322c9d991ab90
Tithin <ttomy@uni-bremen.de>
2023-02-23 00:18:15 +0100

    index html funktioniert

--- src/main/java/com/example/gruppebjava/application/rest/HomeController.java
+++ src/main/java/com/example/gruppebjava/application/rest/HomeController.java
@@ -2,13 +2,15 @@ package com.example.gruppebjava.application.rest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+
 public class HomeController {
 
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "index.html";
     }
 }
 
