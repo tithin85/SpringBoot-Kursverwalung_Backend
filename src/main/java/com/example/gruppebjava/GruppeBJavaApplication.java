0dcb51da426779b1db69fcc9c2f9fe1e5cd29482
th62 <haberland@webtaktik.de>
2023-03-14 13:08:58 +0100

    vorbereitung für live-deployment ;)

--- src/main/java/com/example/gruppebjava/GruppeBJavaApplication.java
+++ src/main/java/com/example/gruppebjava/GruppeBJavaApplication.java
@@ -2,10 +2,16 @@ package com.example.gruppebjava;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class GruppeBJavaApplication {
+public class GruppeBJavaApplication extends SpringBootServletInitializer {
     //GruppeBJavaApplication
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(GruppeBJavaApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(GruppeBJavaApplication.class, args);
