744c556aa5776f06f0aa742c5e8331d60e8d765d
th62 <haberland@webtaktik.de>
2023-03-14 09:52:36 +0100

    user-count für register-link

--- src/main/java/com/example/gruppebjava/security/WebSecurityConfig.java
+++ src/main/java/com/example/gruppebjava/security/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
     http.cors().and().csrf().disable()
         .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
         .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-        .authorizeHttpRequests().requestMatchers("/api/**", "api/auth/**").permitAll()
+        .authorizeHttpRequests().requestMatchers("/api/**", "/api/auth/**").permitAll()
 
             .and()
             .authorizeHttpRequests()
